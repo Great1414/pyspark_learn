69a6bff38461844cbc487a2053ec4674beaceffe
Great <34801728+Great1414@users.noreply.github.com>
2019-05-15 11:42:25 +0800

    Update test_ml.py

--- test_spark/test_ml.py
+++ test_spark/test_ml.py
@@ -2,8 +2,6 @@
 # encoding: utf-8
 '''
 @author: Ren
-@license: (C) Copyright 2019-2020, 深圳市通用互联科技有限责任公司. 
-@contact: renyw@gidomino.com
 @file: test_ml.py
 @time: 2019-05-08 18:22
 @desc: 在此写上代码文件的功能描述
